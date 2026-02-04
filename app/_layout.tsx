9c5546127ee15023944edc87f22c1665c3cdfa18
WooJin Kong <kongwoojin03@gmail.com>
2026-02-04 19:58:50 +0900

    feat: Implement notification receive logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,43 @@
 import { Stack, useRouter } from 'expo-router';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getForceUpdate, appVersion, versionToNumber } from '../services/forceupdate';
+import * as Notifications from 'expo-notifications';
+import { registerForPushNotificationsAsync } from '../services/notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function RootLayout() {
   const router = useRouter();
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ''))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    const responseListener = Notifications.addNotificationResponseReceivedListener((response) => {
+      const data = response.notification.request.content.data.path;
+      if (typeof data === 'string') {
+        router.push(`/webview/${encodeURIComponent(data)}`);
+      }
+    });
+
+    return () => {
+      responseListener.remove();
+    };
+  }, [router]);
+
+  useEffect(() => {
+    //TODO: Send the token to backend server
+    console.log('Expo Push Token:', expoPushToken);
+  }, [expoPushToken]);
 
   useEffect(() => {
     const checkVersion = async () => {
