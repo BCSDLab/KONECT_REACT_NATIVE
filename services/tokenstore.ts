2433a3ccfea08a0aab1c81a9e2e1d3f5aa794863
WooJin Kong <kongwoojin03@gmail.com>
2026-02-22 12:24:34 +0900

    add: Add remove tokens function

--- services/tokenstore.ts
+++ services/tokenstore.ts
@@ -15,3 +15,8 @@ export async function getAccessToken() {
 export async function getRefreshToken() {
   return await SecureStore.getItemAsync(REFRESH_TOKEN_KEY);
 }
+
+export async function removeTokens() {
+  await SecureStore.deleteItemAsync(ACCESS_TOKEN_KEY);
+  await SecureStore.deleteItemAsync(REFRESH_TOKEN_KEY);
+}
